14a115061ad47c4cf045370ef29c1190f5026c35
Christian <chrtaw@outlook.com>
2023-07-31 23:49:17 -0600

    problem 11

---
+++ 11. Container With Most Water/CWMW.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxarea = 0
+
+        l,r = 0, len(height)-1
+
+        while l<r:
+            area = (r-l) * min(height[r],height[l])
+            maxarea = max(maxarea,area)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return maxarea
